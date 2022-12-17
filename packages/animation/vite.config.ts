b1d680c8a0d03a2f25c3da1e0d879d3ac4a85c6d
Chanhee Jang <aksaksdm@naver.com>
2022-12-17 21:40:26 +0900

    init: 리액트 애니메이션 웹 셋업

---
+++ packages/animation/vite.config.ts
@@ -0,0 +1,17 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  server: {
+    port: 4200,
+    host: 'localhost',
+  },
+  plugins: [
+    react(),
+    viteTsConfigPaths({
+      root: '../../',
+    }),
+  ]
+});
